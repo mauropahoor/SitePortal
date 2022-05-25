bb9ea3d21e9cf1548150ea57307ec4e0d897233e
Mauro <mauro.pahoor@gmail.com>
2022-05-25 09:29:33 -0300

    FirestoreConnect

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'siteportal157',
+    appId: '1:707661137178:web:ecf440a6a9a9e56cdbd8a3',
+    databaseURL: 'https://siteportal157-default-rtdb.firebaseio.com',
+    storageBucket: 'siteportal157.appspot.com',
+    apiKey: 'AIzaSyBMFGAx-UYEyV4gFB970SlR6LfV1R9VFqo',
+    authDomain: 'siteportal157.firebaseapp.com',
+    messagingSenderId: '707661137178',
+    measurementId: 'G-G9J3YSL6QZ',
+  },
   production: true
 };
